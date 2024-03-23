22cbe209ef94350e41667497caeace59dbec7c34
victor <victormuhoro7@gmail.com>
2024-03-24 01:31:59 +0300

    Done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if isinstance(pet.owner, Owner) and pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Only instances of Pet can be added as pets.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(self.pets(), key=lambda x: x.name)
+        return sorted_pets
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type. Please choose from: {}".format(self.PET_TYPES))
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.all.append(self)
+
+
